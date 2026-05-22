8e64991aaf474381fa7029cb999722e26bebdee7
José Luis Gutierrez <joselugz03@gmail.com>
2026-05-22 20:13:17 -0300

    add alias import

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,20 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const importPlugin = requite("eslint-plugin-import");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*"],
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: true,
+      },
+    },
   },
 ]);
