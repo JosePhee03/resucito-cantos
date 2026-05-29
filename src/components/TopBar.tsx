67abd4d1df7e473c89369c7f5337fc43c1625191
José Luis Gutierrez <joselugz03@gmail.com>
2026-05-29 07:03:16 -0300

    refactor topbar

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,95 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Icon from "./Icon";
+import { colors, fonts, spacing, typography } from "@/themes";
+import { router } from "expo-router";
+
+export default function TopBar({ title }: { title: string }) {
+  return (
+    <View style={styles.topBar}>
+      <ColLeft />
+      <ColCenter title={title} />
+      <ColRight />
+    </View>
+  );
+}
+
+function ColLeft() {
+  return (
+    <View style={styles.columnLeft}>
+      <TouchableOpacity onPress={router.back} style={{ flexDirection: "row" }}>
+        <Icon size={24} name="chevron-left" color={colors.primary} />
+        <Text style={styles.textIcon}>Átras</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function ColCenter({ title }: { title: string }) {
+  return (
+    <View style={styles.columnCenter}>
+      <Text
+        style={{
+          color: colors.text,
+          fontFamily: fonts.semibold,
+          fontSize: typography.sm,
+        }}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+}
+
+function ColRight() {
+  return (
+    <View style={styles.columnRight}>
+      <TouchableOpacity>
+        <Text style={styles.textIcon}>Filtros</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    borderBottomWidth: 1,
+    borderColor: colors.border,
+    backgroundColor: colors.background,
+    paddingBottom: spacing.sm,
+  },
+  topBar: {
+    height: 48,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: spacing.sm,
+    zIndex: 100,
+  },
+  textIcon: {
+    fontFamily: fonts.medium,
+    fontSize: typography.sm,
+    color: colors.primary,
+    textAlignVertical: "center",
+  },
+  columnLeft: {
+    width: "50%",
+    justifyContent: "center",
+  },
+  columnRight: {
+    width: "50%",
+    height: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingRight: spacing.sm,
+  },
+  columnCenter: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    alignItems: "center",
+  },
+  rowCenter: {
+    flexDirection: "row",
+  },
+});
--- src/components/search/SearchTopBar.tsx
+++ src/components/search/SearchTopBar.tsx
@@ -1,8 +1,7 @@
-import { router } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-import { colors, fonts, spacing, typography } from "@/themes";
-import Icon from "../Icon";
+import { colors, spacing } from "@/themes";
+import TopBar from "../TopBar";
 
 type SearchTopBarProps = {
   title: string;
@@ -12,52 +11,11 @@ type SearchTopBarProps = {
 export default function SearchTopBar({ title, children }: SearchTopBarProps) {
   return (
     <View style={styles.header}>
-      <View style={styles.toolBar}>
-        <ColLeft />
-        <ColCenter title={title} />
-        <ColRight />
-      </View>
-      <View style={{ paddingHorizontal: spacing.md }}>{children}</View>
+      <TopBar title={title} />
+      <View style={styles.searchContainer}>{children}</View>
     </View>
   );
 }
-
-const ColLeft = () => {
-  return (
-    <View style={styles.columnLeft}>
-      <TouchableOpacity onPress={router.back} style={{ flexDirection: "row" }}>
-        <Icon size={24} name="chevron-left" color={colors.primary} />
-        <Text style={styles.textIcon}>Átras</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const ColCenter = ({ title }: { title: string }) => {
-  return (
-    <View style={styles.columnCenter}>
-      <Text
-        style={{
-          color: colors.text,
-          fontFamily: fonts.bold,
-          fontSize: typography.sm,
-        }}
-      >
-        {title}
-      </Text>
-    </View>
-  );
-};
-
-const ColRight = () => {
-  return (
-    <View style={styles.columnRight}>
-      <TouchableOpacity>
-        <Text style={styles.textIcon}>Filtros</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   header: {
@@ -66,39 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
     paddingBottom: spacing.sm,
   },
-  toolBar: {
-    height: 48,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: spacing.sm,
-    zIndex: 100,
-  },
-  textIcon: {
-    fontFamily: fonts.medium,
-    fontSize: typography.sm,
-    color: colors.primary,
-    textAlignVertical: "center",
-  },
-  columnLeft: {
-    width: "50%",
-    justifyContent: "center",
-  },
-  columnRight: {
-    width: "50%",
-    height: "100%",
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    paddingRight: spacing.sm,
-  },
-  columnCenter: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    alignItems: "center",
-  },
-  rowCenter: {
-    flexDirection: "row",
+  searchContainer: {
+    paddingHorizontal: spacing.md,
   },
 });
