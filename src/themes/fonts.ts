e396faebbd1a1a73555d4aef3898100846fb29cb
José Luis Gutierrez <joselugz03@gmail.com>
2026-05-29 07:03:29 -0300

    add font semi bold

--- src/themes/fonts.ts
+++ src/themes/fonts.ts
@@ -1,13 +1,14 @@
 export const fonts = {
   regular: "LibreFranklin-Regular",
   medium: "LibreFranklin-Medium",
+  semibold: "LibreFranklin-SemiBold",
   bold: "LibreFranklin-Bold",
 };
 
 const FONT_SOURCE = {
   "LibreFranklin-Regular": require("../../assets/fonts/LibreFranklin/LibreFranklin-Regular.ttf"),
   "LibreFranklin-Medium": require("../../assets/fonts/LibreFranklin/LibreFranklin-Medium.ttf"),
+  "LibreFranklin-SemiBold": require("../../assets/fonts/LibreFranklin/LibreFranklin-SemiBold.ttf"),
   "LibreFranklin-Bold": require("../../assets/fonts/LibreFranklin/LibreFranklin-Bold.ttf"),
 };
-
 export default FONT_SOURCE;
