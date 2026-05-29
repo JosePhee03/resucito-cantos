d3aa42868d2c99a664b70ea0179734064fd29deb
José Luis Gutierrez <joselugz03@gmail.com>
2026-05-29 16:26:46 -0300

    update search bar

---
+++ src/components/search/SearchBar.tsx
@@ -0,0 +1,103 @@
+import { useRef } from "react";
+import { Pressable, StyleSheet, TextInput, View } from "react-native";
+
+import { colors, fonts, radius, spacing, typography } from "@/themes";
+import Icon from "../Icon";
+
+type SearchBarProps = {
+  onChange: (query: string) => void;
+  query: string;
+  onClear: () => void;
+};
+
+export default function SearchBar({
+  onChange,
+  query,
+  onClear,
+}: SearchBarProps) {
+  const searchInputRef = useRef<TextInput>(null);
+
+  const handleOnSearch = () => {
+    const ref = searchInputRef.current;
+    if (ref?.isFocused) {
+      ref.blur();
+    }
+    ref?.focus();
+  };
+
+  const handleOnClear = () => {
+    onClear();
+  };
+
+  const handleOnChange = (text: string) => {
+    onChange(text);
+  };
+
+  return (
+    <View style={styles.searchInput}>
+      <Pressable
+        onPress={handleOnSearch}
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed,
+        ]}
+      >
+        <Icon name="search" size={24} color={colors.foregroundSecondary} />
+      </Pressable>
+      <TextInput
+        ref={searchInputRef}
+        onChangeText={handleOnChange}
+        value={query}
+        placeholder="Buscar"
+        placeholderTextColor={colors.foregroundSecondary}
+        style={styles.searchText}
+        returnKeyType="search"
+        accessible
+        accessibilityLabel="Campo de búsqueda"
+        accessibilityHint="Escribí para buscar cantos"
+        accessibilityRole="search"
+        autoCorrect={false}
+        autoCapitalize="none"
+        clearButtonMode="never"
+      />
+      {query !== "" && <ButtonClear onClear={handleOnClear} />}
+    </View>
+  );
+}
+
+function ButtonClear({ onClear }: { onClear: () => void }) {
+  return (
+    <Pressable
+      onPress={onClear}
+      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+    >
+      <Icon name="close" size={24} color={colors.foregroundSecondary} />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: spacing.sm,
+    height: "100%",
+    justifyContent: "center",
+    borderRadius: radius.lg,
+  },
+  searchInput: {
+    height: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: radius.sm,
+    backgroundColor: colors.surfaceSecondary,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    backgroundColor: colors.pressed,
+  },
+  searchText: {
+    flex: 1,
+    fontFamily: fonts.regular,
+    fontSize: typography.md,
+    color: colors.text,
+  },
+});
