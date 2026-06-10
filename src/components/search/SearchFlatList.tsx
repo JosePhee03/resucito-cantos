9987bda7393ac3fc89bd4fbb016a4e5fa747937c
José Luis Gutierrez <joselugz03@gmail.com>
2026-06-10 12:32:17 -0300

    update search flat list component

--- src/components/search/SearchFlatList.tsx
+++ src/components/search/SearchFlatList.tsx
@@ -1,59 +1,190 @@
-import { memo, useEffect, useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { memo } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  SharedValue,
+  useAnimatedScrollHandler,
+} from "react-native-reanimated";
 
-import { colors } from "@/themes";
+import { colors, fonts, radius, spacing, typography } from "@/themes";
 import { Song } from "@/domain/song";
-import SearchFooter from "./SearchFooter";
 import SearchEmplyList from "./SearchEmplyList";
 import SongItem from "./SongItem";
-import useSongDebounce from "@/hooks/useSongDebounce";
+import Icon from "../Icon";
 
 type SongFlatListProps = {
-  query: string;
-  stage?: string;
+  songs: Song[];
+  title?: string;
   onPressItem: (id: string) => void;
+  headerHidden: SharedValue<boolean>;
+  showList: boolean;
 };
 
 export default function SongFlatList({
-  query,
-  stage,
+  songs,
+  title = "",
   onPressItem,
+  headerHidden,
+  showList,
 }: SongFlatListProps) {
-  const { songs, loading } = useSongDebounce(query, stage, 300);
-  const [showList, setShowList] = useState(false);
-
-  useEffect(() => {
-    const id = requestAnimationFrame(() => {
-      setShowList(true);
-    });
-
-    return () => cancelAnimationFrame(id);
-  }, []);
+  const onScroll = useAnimatedScrollHandler((event) => {
+    const isHidden = event.contentOffset.y > 52;
+    headerHidden.value = isHidden;
+  });
 
   if (!showList) {
     return null;
   }
 
   return (
-    <>
-      <FlatList
-        style={styles.list}
-        contentContainerStyle={styles.listContent}
-        keyboardShouldPersistTaps="handled"
-        data={songs}
-        ListEmptyComponent={() => !loading && <SearchEmplyList />}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <SongItemMemo song={item} onPress={onPressItem} />
-        )}
-        removeClippedSubviews
-        initialNumToRender={10}
-        maxToRenderPerBatch={10}
-        windowSize={5}
-      />
-      <View style={styles.spacer}></View>
-      <SearchFooter total={songs.length} loading={loading} />
-    </>
+    <Animated.FlatList
+      ListHeaderComponent={
+        <View style={{ backgroundColor: colors.background }}>
+          <View
+            style={{
+              paddingHorizontal: spacing.md,
+              paddingVertical: spacing.sm,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: fonts.bold,
+                fontSize: typography.lg,
+                color: colors.text,
+              }}
+            >
+              {title}
+            </Text>
+            <Text
+              style={{
+                fontFamily: fonts.regular,
+                fontSize: typography.sm,
+                color: colors.textSecondary,
+              }}
+            >
+              {`${songs.length} Cantos`}
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingHorizontal: spacing.sm,
+            }}
+          >
+            <Pressable
+              style={({ pressed }) => [
+                {
+                  flexDirection: "row",
+                  paddingHorizontal: spacing.sm,
+                  gap: spacing.xs,
+                  alignItems: "center",
+                  height: 32,
+                  borderRadius: radius.lg,
+                },
+                pressed && { backgroundColor: colors.pressed },
+              ]}
+            >
+              <Text
+                style={{
+                  fontFamily: fonts.regular,
+                  fontSize: typography.sm,
+                  color: colors.foreground,
+                }}
+              >
+                Filtros
+              </Text>
+              <Icon
+                name="chevrons-up-down"
+                size={16}
+                color={colors.foreground}
+              />
+            </Pressable>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Pressable
+                style={({ pressed }) => [
+                  {
+                    flexDirection: "row",
+                    paddingHorizontal: spacing.sm,
+                    gap: spacing.xs,
+                    alignItems: "center",
+                    height: 32,
+                    borderStartStartRadius: radius.lg,
+                    borderBottomStartRadius: radius.lg,
+                  },
+                  pressed && { backgroundColor: colors.pressed },
+                ]}
+              >
+                <Text
+                  style={{
+                    fontFamily: fonts.regular,
+                    fontSize: typography.sm,
+                    color: colors.foreground,
+                  }}
+                >
+                  Vista
+                </Text>
+                <Icon name="layout-list" size={16} color={colors.foreground} />
+              </Pressable>
+              <View
+                style={{
+                  height: 20,
+                  width: 1,
+                  backgroundColor: colors.foreground,
+                }}
+              />
+              <Pressable
+                style={({ pressed }) => [
+                  {
+                    flexDirection: "row",
+                    paddingHorizontal: spacing.sm,
+                    gap: spacing.xs,
+                    alignItems: "center",
+                    height: 32,
+                    borderEndEndRadius: radius.lg,
+                    borderTopEndRadius: radius.lg,
+                  },
+                  pressed && { backgroundColor: colors.pressed },
+                ]}
+              >
+                <Text
+                  style={{
+                    fontFamily: fonts.regular,
+                    fontSize: typography.sm,
+                    color: colors.foreground,
+                  }}
+                >
+                  Ordenar
+                </Text>
+                <Icon
+                  name="arrow-down-up"
+                  size={16}
+                  color={colors.foreground}
+                />
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      }
+      onScroll={onScroll}
+      style={styles.list}
+      contentContainerStyle={styles.listContent}
+      keyboardShouldPersistTaps="handled"
+      data={songs}
+      ListEmptyComponent={<SearchEmplyList />}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <SongItemMemo song={item} onPress={onPressItem} />
+      )}
+      removeClippedSubviews
+      initialNumToRender={10}
+      maxToRenderPerBatch={10}
+      windowSize={5}
+    />
   );
 }
 
