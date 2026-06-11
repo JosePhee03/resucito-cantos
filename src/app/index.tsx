4255ff8310698030d846e4710a43c35048cb3f69
José Luis Gutierrez <joselugz03@gmail.com>
2026-06-11 18:38:16 -0300

    refactor button search

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ import { router, useFocusEffect } from "expo-router";
 import { colors, fonts, radius, spacing, typography, CONSTANT } from "@/themes";
 import { Stage } from "@/domain/song";
 import { SectionIndex } from "@/components/home";
-import { Icon } from "@/components";
+import { ButtonSearch, Icon } from "@/components";
 import { useRef } from "react";
 
 export default function HomeScreen() {
@@ -57,39 +57,7 @@ function Header() {
   );
 }
 
-function ButtonSearch({ onPress }: { onPress: () => void }) {
-  return (
-    <Pressable
-      onPress={() => onPress()}
-      style={({ pressed }) => [
-        styles.buttonSearch,
-        pressed && styles.buttonPressed,
-      ]}
-    >
-      <Icon name="search" size={24} color={colors.foregroundSecondary} />
-      <Text style={styles.buttonSearchText}>Buscar cantos</Text>
-    </Pressable>
-  );
-}
-
 const styles = StyleSheet.create({
-  buttonSearch: {
-    height: CONSTANT.SEARCHBAR,
-    flexDirection: "row",
-    alignItems: "center",
-    gap: spacing.sm,
-    paddingHorizontal: spacing.sm,
-    borderRadius: radius.lg,
-    backgroundColor: colors.surfaceSecondary,
-  },
-  buttonSearchText: {
-    fontFamily: fonts.regular,
-    fontSize: typography.md,
-    color: colors.foregroundSecondary,
-  },
-  buttonPressed: {
-    backgroundColor: colors.pressed,
-  },
   container: {
     flex: 1,
     backgroundColor: colors.background,
