f3f3bead6f7a7f34e7075d62541500905e4edc8c
José Luis Gutierrez <joselugz03@gmail.com>
2026-06-12 22:07:43 -0300

    update search modal screen

--- src/app/search-modal.tsx
+++ src/app/search-modal.tsx
@@ -1,64 +1,46 @@
 import { SearchBar } from "@/components/search";
 import { colors, CONSTANT, fonts, spacing, typography } from "@/themes";
-import { router } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { KeyboardAvoidingView } from "react-native-keyboard-controller";
+import { ButtonText, TopBar } from "@/components";
 
 export default function SearchModalScreen() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
-      <View style={styles.header}>
-        <Pressable
-          onPress={router.back}
-          style={({ pressed }) => [
-            styles.button,
-            pressed && styles.buttonPressed,
-          ]}
-        >
-          <Text style={styles.buttonText}>Cancelar</Text>
-        </Pressable>
-        <Pressable
-          style={({ pressed }) => [
-            styles.button,
-            pressed && styles.buttonPressed,
-          ]}
-        >
-          <Text style={styles.buttonText}>Buscar</Text>
-        </Pressable>
-      </View>
-      <View
-        style={{ paddingHorizontal: spacing.md, paddingVertical: spacing.sm }}
-      >
-        <SearchBar
-          loading={false}
-          onCancel={() => {}}
-          onChange={() => {}}
-          query=""
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior={"padding"}>
+        <TopBar
+          right={
+            <ButtonText text="Buscar" px="sm" size="sm" onPress={() => {}} />
+          }
         />
-      </View>
-      <View style={{ paddingHorizontal: spacing.md }}>
-        <Text
-          style={{
-            fontFamily: fonts.semibold,
-            fontSize: typography.md,
-            color: colors.text,
-          }}
-        >
-          Busquedas recientes
-        </Text>
-      </View>
+        <View style={styles.searchContainer}>
+          <SearchBar onSubmit={() => {}} onChange={() => {}} query="" />
+        </View>
+        <View style={styles.content}>
+          <View style={styles.sectionHeader}>
+            <Text style={styles.title}>Búsquedas recientes</Text>
+            <ButtonText text="Limpiar" size="sm" onPress={() => {}} />
+          </View>
+          <View style={styles.emptySuggestion}>
+            <Text style={styles.emptySuggestionText}>
+              Sin búsquedas recientes
+            </Text>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: spacing.sm,
-    height: CONSTANT.HEADER,
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  searchContainer: {
+    paddingHorizontal: spacing.md,
+    paddingVertical: spacing.sm,
   },
   button: {
     paddingHorizontal: spacing.sm,
@@ -72,5 +54,35 @@ const styles = StyleSheet.create({
     fontFamily: fonts.medium,
     fontSize: typography.sm,
     color: colors.primary,
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: spacing.md,
+    paddingBottom: spacing.md,
+  },
+  title: {
+    fontFamily: fonts.medium,
+    fontSize: typography.sm,
+    color: colors.textSecondary,
+  },
+  sectionHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  clearText: {
+    fontFamily: fonts.regular,
+    fontSize: typography.sm,
+    color: colors.primary,
+  },
+  emptySuggestion: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  emptySuggestionText: {
+    fontFamily: fonts.medium,
+    fontSize: typography.sm,
+    color: colors.textTertiary,
+    textAlign: "center",
   },
 });
