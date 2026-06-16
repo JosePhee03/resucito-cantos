06633f9b5c715546585325489dd07fd71436ac3f
José Luis Gutierrez <joselugz03@gmail.com>
2026-06-15 22:13:49 -0300

    remove export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Icon } from "./Icon";
 export { default as TopBar } from "./TopBar";
-export { default as ButtonSearch } from "./ButtonSearch";
+export { default as SearchBar } from "./SearchBar";
+export { default as SearchBottomSheetModal } from "./SearchBottomSheetModal";
+export { default as SearchBottomSheet } from "./SearchBottomSheet";
 export * from "./Button";
--- src/components/search/index.ts
+++ src/components/search/index.ts
@@ -1,3 +1,2 @@
 export { default as SearchTopBar } from "./SearchTopBar";
-export { default as SearchBar } from "./SearchBar";
 export { default as SearchFlatList } from "./SearchFlatList";
