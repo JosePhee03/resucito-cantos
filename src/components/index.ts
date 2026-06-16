d9f035102883e278af48e858480374cba38824aa
José Luis Gutierrez <joselugz03@gmail.com>
2026-06-16 10:49:37 -0300

    remove search button sheet

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
 export { default as Icon } from "./Icon";
 export { default as TopBar } from "./TopBar";
 export { default as SearchBar } from "./SearchBar";
-export { default as SearchBottomSheetModal } from "./SearchBottomSheetModal";
-export { default as SearchBottomSheet } from "./SearchBottomSheet";
 export * from "./Button";
